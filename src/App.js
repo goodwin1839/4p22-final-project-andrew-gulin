c2184822d05b1de90132a8f475477c0680d61c6a
Andrew Gulin <andreygulin@astroshock.ru>
2022-11-18 20:46:43 +0300

    add app and data.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
+import { useEffect, useState } from 'react';
+import Counter from './components/Counter/Counter';
 
 function App() {
+  const [ products, setProducts ] = useState([]);
+
+  useEffect(() => {
+      console.log('useEffect');
+      fetch('/4p22-final-project-andrew-gulin/data.json')
+        .then((response) => response.json())
+        .then((result) => {
+          setProducts(result);
+        });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <Counter/>
+      <div className="App-container">
+        {
+          products.map((item, index) => {
+            return <Card key={index} title={item.title} 
+                         description={item.description}
+                         onClick={(event) => console.log('click on', event)}
+                         img={item.image}
+                         price={item.price}/>
+          })
+        }
+      </div>
+      <div className="App-newsletter">
+        <h2>Подписаться на новости компании</h2>
+        <Input type="email" placeholder="Введите email" name="email" id="email" />
+        <Button>Подписаться</Button>
+      </div>
     </div>
   );
 }
